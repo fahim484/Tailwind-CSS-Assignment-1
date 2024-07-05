cd02eb8d9b06fe83597eba4af1b0bb7aceb9abc1
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-07-05 17:35:29 +0600

    Hero or Banner section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*'],
+  content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jakarta: ['"Plus Jakarta Sans", sans-serif'],
+        inter: ['"Inter", sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
